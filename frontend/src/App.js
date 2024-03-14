e47acab3f83901c10dfbd43691818e10c40be25c
MikeM911 <80363062+MikeM911@users.noreply.github.com>
2024-03-14 15:04:25 -0400

    installed react-icons and included font libraries

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@
 function App() {
   return (
     <div>
-      <h1 classname="text-3xl font-bold underline">Hello World</h1>
+      <h1 classname="text-4xl font-bold underline">Hello World</h1>
     </div>
   );
 }
